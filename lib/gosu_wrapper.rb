299116a299f7f830b8c2c0749a00ddbc3cecd4fa
Kyle Whittington <kthomaswhittington@gmail.com>
2015-12-27 00:12:51 -0600

    Add lib/gosu_wrapper.rb and lib/gosu_wrapper/version.rb

---
+++ lib/gosu_wrapper.rb
@@ -0,0 +1,5 @@
+require 'gosu_wrapper/version'
+
+# A wrapper for the Gosu Ruby library
+module GosuWrapper
+end
---
+++ lib/gosu_wrapper/version.rb
@@ -0,0 +1,4 @@
+# A wrapper for the Gosu Ruby library
+module GosuWrapper
+  VERSION = '0.1.0'
+end
