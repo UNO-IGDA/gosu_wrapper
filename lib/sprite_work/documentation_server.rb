f39bac580c13569d9bafd71dab17e324bca10ac8
Kyle Whittington <kthomaswhittington@gmail.com>
2016-01-04 10:43:20 -0600

    Add SpriteWork::DocumentationServer.start

--- lib/sprite_work/documentation_server.rb
+++ lib/sprite_work/documentation_server.rb
@@ -4,5 +4,12 @@ module SpriteWork
   # A small server application to host live documentation.
   class DocumentationServer
     include Singleton
+
+    def self.start
+      instance.start
+    end
+
+    def start
+    end
   end
 end
--- spec/lib/sprite_work/documentation_server_spec.rb
+++ spec/lib/sprite_work/documentation_server_spec.rb
@@ -11,5 +11,15 @@ module SpriteWork
     subject(:documentation_server) { described_class.instance }
 
     it('is a Singleton') { is_expected.to be_a Singleton }
+
+    describe '.start' do
+      subject(:start) { described_class.start }
+
+      it 'calls instance.start' do
+        expect(documentation_server).to receive(:start)
+
+        start
+      end
+    end
   end
 end
